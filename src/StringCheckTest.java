6bc184c89ea2d87ea0d2fac6637c42a3bf2191e5
KunigiMohan <kmohanr5@gmail.com>
2021-11-11 10:21:14 +0530

    Added : Test class for checking max() method.

--- src/StringCheckTest.java
+++ src/StringCheckTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StringCheckTest {
-  
+/*
+* Test Case for checking method
+* */
+    @Test
+    void max() {
+        StringCheck stringCheck= new StringCheck();
+        //giving inputs to test case
+        assertEquals("peach",stringCheck.max("apple","peach","banana",0));
+        assertEquals("peach",stringCheck.max("apple","peach","banana",1));
+        assertEquals("peach",stringCheck.max("apple","peach","banana",2));//checking maximum value is at correct index.
+    }
 }
