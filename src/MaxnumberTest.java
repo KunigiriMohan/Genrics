c35465058767e30671f3f57e12207a76ab33a1a0
KunigiMohan <kmohanr5@gmail.com>
2021-11-11 09:04:35 +0530

    Added : Test class for testing max method.

--- src/MaxnumberTest.java
+++ src/MaxnumberTest.java
@@ -1,4 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MaxnumberTest {
-  
+
+    @Test
+    void max() {
+        Maxnumber maxnumber= new Maxnumber();
+                        //giving inputs to test case
+        assertEquals(200,maxnumber.max(50,200,100,0));                                //checking maximum value is at correct index.
+    }
 }
